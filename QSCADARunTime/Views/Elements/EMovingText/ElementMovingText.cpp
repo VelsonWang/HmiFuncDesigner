37bf2097bba50c9874252c4937f4c3a6ad3d830b
JasonWang <706409617@qq.com>
2019-07-28 08:15:06 +0800

    add ElementMovingText in runtime.

--- QSCADARunTime/Views/Elements/EMovingText/ElementMovingText.cpp
+++ QSCADARunTime/Views/Elements/EMovingText/ElementMovingText.cpp
@@ -57,12 +57,6 @@ void ElementMovingText::paint(QPainter *painter)
     painter->translate(QPoint(elementXPos, elementYPos));
     painter->rotate(elemAngle);
 
-    // 背景色不透明显示
-    if(!transparentBackground_) {
-        QBrush brush(backgroundColor_);
-        painter->fillRect(elementRect_, brush);
-    }
-
     // 绘制移动文本
     drawMovingText(painter);
 
@@ -85,28 +79,54 @@ void ElementMovingText::drawMovingText(QPainter *painter)
     QString szText = "";
     QSize textSize;
 
-    painter->setPen(textColor);
-    painter->setBrush(Qt::NoBrush);
-    painter->setFont(font_);
 
     QRectF rect(elementRect_.toRect());
     QRectF eleRect = rect.normalized().adjusted(borderWidth_, borderWidth_, -borderWidth_, -borderWidth_);
     int iEleRectWidth = static_cast<int>(eleRect.width());
     int iEleRectHeight = static_cast<int>(eleRect.height());
 
-    if(szTagSelected_ == "") { // 未关联变量, 使用属性：移动文本
-        szText = elementText;
+    QColor getBackgroundColor;
+    QColor getTextColor;
 
-    } else { // 已关联变量, 使用属性：变量文本列表
+    // 未关联变量, 使用属性：移动文本
+    szText = elementText;
+    getBackgroundColor = backgroundColor_;
+    getTextColor = textColor;
 
+    // 已关联变量, 使用属性：变量文本列表
+    if(szTagSelected_ != "") {
+        QString szTagValue = "#";
         QString szTagID = RealTimeDB::getIdByTagName(szTagSelected_);
         if (szTagID != "") {
-            elementText = RealTimeDB::GetDataString(szTagID);
+            szTagValue = RealTimeDB::GetDataString(szTagID);
         } else {
-            elementText = "#";
+            szTagValue = "#";
         }
 
+        for(int i=0; i<tagTextList_.size(); i++) {
+            QString szValueColor = tagTextList_.at(i);
+            QStringList listValueColor = szValueColor.split(":");
+            if(listValueColor.size() == 4) {
+                if(szTagValue != "#") {
+                    if(szTagValue == listValueColor.at(0)) {
+                        szText = listValueColor.at(1);
+                        getBackgroundColor = QColor(listValueColor.at(2));
+                        getTextColor = QColor(listValueColor.at(3));
+                    }
+                }
+            }
+        }
     }
+
+    // 背景色不透明显示
+    if(!transparentBackground_) {
+        QBrush brush(getBackgroundColor);
+        painter->fillRect(elementRect_, brush);
+    }
+
+    painter->setPen(getTextColor);
+    painter->setBrush(Qt::NoBrush);
+    painter->setFont(font_);
 
     QFontMetrics fontMetrics(font_);
     textSize = fontMetrics.size(Qt::TextSingleLine, elementText);
@@ -117,87 +137,60 @@ void ElementMovingText::drawMovingText(QPainter *painter)
         textRect_.setTop((iEleRectHeight - textSize.height()) / 2);
         textRect_.setHeight(textSize.height());
 
+        if(szText.indexOf(szMovingText_) == -1) {
+            szMovingText_ = szText;
+        }
+
         if(szMoveDir_ == "LeftToRight") { // 从左到右
+            iOneCharWidth_ = textSize.width() / elementText.length();
+            iPos_ += (iOneCharWidth_ * iMoveCharNum_);
 
+            if(iPos_ >= (0 - iOneCharWidth_ * szText.length()) && iPos_ < textSize.width()) {
+                if(iPos_ < 0) {
+                    textRect_.setLeft(0);
+                } else {
+                    textRect_.setLeft(iPos_);
+                }
+                QString szInsert = szText.mid(szText.length() - szMovingText_.length() - iMoveCharNum_, iMoveCharNum_);
+                szMovingText_.insert(0, szInsert);
+                textRect_.setWidth(szMovingText_.length() * iOneCharWidth_);
+            } else if(iPos_ >= textSize.width() && iPos_ <= (iEleRectWidth - textSize.width())) {
+                textRect_.setLeft(iPos_);
+                textRect_.setWidth(textSize.width());
+            } else if(iPos_ > (iEleRectWidth - textSize.width()) && iPos_ <= iEleRectWidth) {
+                textRect_.setLeft(iPos_);
+                szMovingText_ = szMovingText_.remove(szMovingText_.length() - iMoveCharNum_, iMoveCharNum_);
+                textRect_.setWidth(szMovingText_.length() * iOneCharWidth_);
+            } else {
+                iPos_ = 0 - iOneCharWidth_ * szText.length();
+                szMovingText_ = "";
+                textRect_.setWidth(0);
+            }
         } else if(szMoveDir_ == "RightToLeft") { // 从右到左
-
             iOneCharWidth_ = textSize.width() / elementText.length();
             int iEleRectWidthAdjusted = 0;
 
             iPos_ -= (iOneCharWidth_ * iMoveCharNum_);
-            if(iPos_ < 0) {
+
+            if(iPos_ <= iEleRectWidth && iPos_ > (iEleRectWidth - textSize.width())) {
+                textRect_.setLeft(iPos_);
+                textRect_.setWidth(iEleRectWidth - iPos_);
+            } else if(iPos_ <= (iEleRectWidth - textSize.width()) && iPos_ >= 0) {
+                textRect_.setLeft(iPos_);
+                textRect_.setWidth(textSize.width());
+            } else if(iPos_ < 0 && iPos_ > -textSize.width()) {
+                szMovingText_ = szMovingText_.remove(0, iMoveCharNum_);
+                textRect_.setWidth(szMovingText_.length() * iOneCharWidth_);
+            } else {
                 int iMod = iEleRectWidth % (iOneCharWidth_ * iMoveCharNum_);
                 iEleRectWidthAdjusted = (iMod > 0) ? (iEleRectWidth - iMod) : iEleRectWidth;
-                iPos_ = iEleRectWidthAdjusted;
+                iPos_ = iEleRectWidthAdjusted + iOneCharWidth_ * iMoveCharNum_;
                 szMovingText_ = szText;
-            }
-
-            textRect_.setLeft(iPos_);
-            if(iPos_ <= iEleRectWidth && iPos_ > (iEleRectWidth - textSize.width())) {
-                textRect_.setWidth(iEleRectWidth - iPos_);
-            } else if(iPos_ <= (iEleRectWidth - textSize.width()) && iPos_ >= textSize.width()) {
-                textRect_.setWidth(textSize.width());
-            } else {
-                qDebug() <<__FILE__ << __LINE__ <<__FUNCTION__
-                        << " iPos_: " << iPos_
-                        << " iOneCharWidth_ : " << iOneCharWidth_
-                        << " iMoveCharNum_: " << iMoveCharNum_
-                        << " iOneCharWidth_ * iMoveCharNum_: " << iOneCharWidth_ * iMoveCharNum_;
-                if(iPos_ >= iOneCharWidth_ * iMoveCharNum_) {
-                    textRect_.setLeft(iPos_);
-                    textRect_.setWidth(textSize.width());
-                } else {
-                    if(iPos_ <= iOneCharWidth_ / 2) {
-                        textRect_.setLeft(0);
-                        if(szMovingText_.length() == szText.length()) {
-                            textRect_.setLeft(iPos_);
-                        } else {
-                            szMovingText_ = szMovingText_.remove(0, iMoveCharNum_);
-                        }
-                    }
-                    textRect_.setWidth(szMovingText_.length() * iOneCharWidth_);
-                    if(szMovingText_.length() > 0) {
-                        iPos_ = iOneCharWidth_ * iMoveCharNum_ + iOneCharWidth_ / 2;
-                    }
-                }
-
-            }
-
-
-
-
-/*
-            int iSub = iEleRectWidth - iPos_;
-            if(iSub < 0) {
-                iPos_ = 0;
-                textRect_.setLeft(iEleRectWidth);
                 textRect_.setWidth(0);
-            } else {
-                if(iSub > textSize.width()) {
-                    textRect_.setLeft(iEleRectWidth - textSize.width() - iPos_);
-                    textRect_.setWidth(textSize.width());
-                } else {
-                    if(iPos_ > textSize.width()) {
-
-                    } else {
-
-                    }
-
-                    szMovingText_ = szMovingText_.remove(0, iMoveCharNum_);
-                    textRect_.setLeft(0);
-                    textRect_.setWidth(szMovingText_.length() * iOneCharWidth_);
-                }
             }
-            */
-        }
-
-        if((szText.length() - szMovingText_.length()) == szText.length() ||
-                szText.length() == szMovingText_.length()) {
-            szMovingText_ = szText;
         }
     }
 
-    painter->fillRect(textRect_, Qt::yellow);
     painter->drawText(textRect_, Qt::AlignLeft, szMovingText_);
 }
 
@@ -240,6 +233,11 @@ void ElementMovingText::readFromXml(const QXmlStreamAttributes &attributes)
 
     if (attributes.hasAttribute("moveDir")) {
         szMoveDir_ = attributes.value("moveDir").toString();
+        if(szMoveDir_ == "LeftToRight") { // 从左到右
+            iPos_ = -32767;
+        } else if(szMoveDir_ == "RightToLeft") { // 从右到左
+            iPos_ = -32767;
+        }
     }
 
     if (attributes.hasAttribute("moveCharNum")) {
@@ -359,6 +357,11 @@ void ElementMovingText::readData(QDataStream &in)
     this->elementText = text;
     szMovingText_ = elementText;
     this->szMoveDir_ = szMoveDir;
+    if(szMoveDir_ == "LeftToRight") { // 从左到右
+        iPos_ = -32767;
+    } else if(szMoveDir_ == "RightToLeft") { // 从右到左
+        iPos_ = -32767;
+    }
     this->iMoveCharNum_ = iMoveCharNum;
     this->period_ = period;
     this->backgroundColor_ = backgroundColor;
@@ -424,6 +427,11 @@ QDataStream &operator>>(QDataStream &in, ElementMovingText &ele)
     ele.elementText = text;
     ele.szMovingText_ = ele.elementText;
     ele.szMoveDir_ = szMoveDir;
+    if(ele.szMoveDir_ == "LeftToRight") { // 从左到右
+        ele.iPos_ = -32767;
+    } else if(ele.szMoveDir_ == "RightToLeft") { // 从右到左
+        ele.iPos_ = -32767;
+    }
     ele.iMoveCharNum_ = iMoveCharNum;
     ele.period_ = period;
     ele.backgroundColor_ = backgroundColor;
